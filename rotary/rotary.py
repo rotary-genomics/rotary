e33733bef27e137220eb94db279f2b58033ceb80
Lee Bergstrand <leemacboy@gmail.com>
2023-10-20 16:24:10 -0700

    Moved FASTQ file extension check to a function. Check extension both in sample creation and during rotary init. Changes so it checks the entire file extension even if there are multiple.

--- rotary/rotary.py
+++ rotary/rotary.py
@@ -14,7 +14,7 @@ import sys
 import psutil
 from ruamel.yaml import YAML
 
-from rotary.sample import SequencingFile, Sample
+from rotary.sample import SequencingFile, Sample, is_fastq_file
 from rotary.utils import check_for_files, get_cli_arg_path
 
 yaml = YAML()
@@ -197,10 +197,10 @@ def init(args, config, output_dir_path):
     input_path = get_cli_arg_path(args, 'input_dir')
     fastq_files = []
 
-    for file in os.listdir(input_path):
-        if 'fastq' in file:
-            file = SequencingFile(file_path=(os.path.join(input_path, file)))
-            fastq_files.append(file)
+    for file_path in os.listdir(input_path):
+        filename = os.path.basename(file_path)
+        if is_fastq_file(filename):
+            fastq_files.append(SequencingFile(file_path=os.path.join(input_path, file_path)))
 
     samples_files = {}
     for file in fastq_files:
