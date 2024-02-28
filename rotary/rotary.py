42b28fe457a14978818e694b38a2d66226bbfb06
Lee Bergstrand <leemacboy@gmail.com>
2024-02-28 15:47:21 -0800

    Revert "move too checking for defaults configs to parent program when using pungi. Corresponds to: https://github.com/rotary-genomics/pungi/commit/f0c9c05c9ab91affc5b19d9aac2b910dc8087703"
    This reverts commit 990585a3969b37ada2d5cb9dae1263d22983f5e5.

--- rotary/rotary.py
+++ rotary/rotary.py
@@ -114,12 +114,8 @@ def init(args):
     """
     output_dir_path = get_cli_arg_path(args, 'output_dir')
 
-    config_path = get_cli_arg_path(args, 'config')
-    if not config_path:  # If no config is specified via CLI grab the default config.yaml.
-        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.yaml')
-
     # Checks for existing run files and sets up the run directory.
-    setup_run_directory(args, output_dir_path, run_files, config_path)
+    setup_run_directory(args, output_dir_path, run_files)
 
     input_path = get_cli_arg_path(args, 'input_dir')
 
