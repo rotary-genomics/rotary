fb31b061a65209f215b6fc3e6d8cee6ec8d43683
Lee Bergstrand <leemacboy@gmail.com>
2024-02-28 15:51:49 -0800

    support moving config creation outside of of setup_run_directory

--- rotary/rotary.py
+++ rotary/rotary.py
@@ -114,8 +114,14 @@ def init(args):
     """
     output_dir_path = get_cli_arg_path(args, 'output_dir')
 
+    config_path = get_cli_arg_path(args, 'config')
+    if not config_path:  # If no config is specified via CLI grab the default config.yaml.
+        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.yaml')
+
+    config = load_yaml_config(config_path)
+
     # Checks for existing run files and sets up the run directory.
-    setup_run_directory(args, output_dir_path, run_files)
+    setup_run_directory(args, output_dir_path, run_files, config)
 
     input_path = get_cli_arg_path(args, 'input_dir')
 
